cc2a25ca3b17d4a83df09d9826c2a3c997f152f3
mstaniak <mateusz.staniak@math.uni.wroc.pl>
2017-04-08 00:24:15 +0200

    docs

---
+++ R/kendallRandomPackage.R
@@ -0,0 +1,15 @@
+#' kendallRandomPackage: package for exploring air pollution data in Poland.
+#'
+#' Package provides functions that help import datasets from .xlsx files provided on GIOŚ website, 
+#' convert them to the form suitable for analyses, calculate maxima over given period,
+#' and visualize them using attached shiny app.
+#' 
+#' @section Important function:
+#' \code{\link{importGiosFromXLSX}} imports data from multiple .xlsx files.
+#' \code{\link{calculateMaxima}} calculates maxima over given period for data important using \code{\link{importGiosFromXLSX}}.
+#' \code{\link{kendallRandomApp}} starts a Shiny application that features multiple tools for exploring obtained data, 
+#' including peak over threshold methods.
+#'
+#' @docType package
+#' @name kendallRandomPackage
+NULL
