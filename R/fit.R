c29e4956dea50d2d64425e6294f79dff121c4dd9
mstaniak <mateusz.staniak@math.uni.wroc.pl>
2017-04-11 00:53:35 +0200

    comparing CDFs

--- R/fit.R
+++ R/fit.R
@@ -16,9 +16,36 @@ qqPlotGev <- function(sourceFrame, fittedGEV) {
     filter(is.finite(maximum)) %>%
     arrange(maximum) %>%
     mutate(no = 1:nrow(.)) %>%
-    mutate(theoretical = qgevd(par1, par2, par3, no/(max(no) + 1)),
+    mutate(theoretical = qgevd(no/(max(no) + 1), par1, par2, par3),
 	   empirical = quantile(maximum, no/(max(no) + 1))) %>%
-    ggplot2(aes(x = theoretical, y = empirical)) +
+    ggplot(aes(x = theoretical, y = empirical)) +
       geom_point() +
       theme_bw()
+}
+
+#' Compare theorical and empirical CDF for fitted GEV distribution.
+#'
+#' @param sourceFrame tibble returned by filteredData()
+#' @param fittedGEV list returned by egevd function
+#'
+#' @return ggplot2 object
+#'
+#' @export
+#'
+
+cdfsGev <- function(sourceFrame, fittedGEV) {
+  sourceFrame %>%
+    mutate(par1 = fittedGEV$parameters[1],
+           par2 = fittedGEV$parameters[2],
+           par3 = fittedGEV$parameters[3]) %>%
+    filter(is.finite(maximum)) %>%
+    arrange(maximum) %>%
+    mutate(no = 1:nrow(.)) %>%
+    mutate(theoretical = pgevd(maximum, par1, par2, par3),
+           empirical = no/max(no)) %>%
+    gather(CDF, value, theoretical, empirical) %>% 
+    ggplot(aes(x = maximum, y = value, color = CDF)) +
+    geom_point() +
+    theme_bw() +
+    ylab("")
 }
