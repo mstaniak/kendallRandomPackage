9fb55f3e032d43f896090b3ae3d3e5b9314cb89b
mstaniak <mateusz.staniak@math.uni.wroc.pl>
2017-04-20 23:17:03 +0200

    fit GEV dist to observations from multiple years/polutants

--- R/fit.R
+++ R/fit.R
@@ -1,3 +1,33 @@
+#' Fit GEV distribution to multiple sets of observations
+#'
+#' @param srcFrame tibble returned by calculateMaxima function
+#' @param groupingVariables chr, vector of names of columns to group by,
+#'        year and polutant by default
+#' @param full lgl, if TRUE, tibble from srcFrame argument will be returned
+#'        with additional columns giving parameter values of fitted GEV dist.
+#'        If FALSE, just parameters (with corresponding values of grouping variables)
+#'        will be returned.
+#' @param ... additional parameters passed to egevd() function
+#'        
+#' @return tibble as described in help for full argument
+#'
+#' @export
+#'
+
+fitMultiGEV <- function(srcFrame, groupingVariables = c("year", "polutant"), full = TRUE) {
+  tmp <- srcFrame %>%
+    dplyr::filter(is.finite(maximum)) %>%
+    dplyr::group_by_(.dots = groupingVariables) %>%
+    dplyr::summarise(location = egevd(maximum, ...)$parameters[1],
+                     scale = egevd(maximum, ...)$parameters[2],
+                     shape = egevd(maximum, ...)$parameters[3])
+  if(full) srcFrame %>%
+    left_join(tmp, by = groupingVariables) %>%
+    arrange_(.dots = groupingVariables)
+  else tmp
+}
+
+
 #' QQ-plot for fited GEV distribution
 #' 
 #' @param sourceFrame tibble returned by filteredData()
@@ -8,14 +38,14 @@
 
 qqPlotGev <- function(sourceFrame, fittedGEV) {
   sourceFrame %>%
-    mutate(par1 = fittedGEV$parameters[1],
-	   par2 = fittedGEV$parameters[2],
-	   par3 = fittedGEV$parameters[3]) %>%
-    filter(is.finite(maximum)) %>%
-    arrange(maximum) %>%
-    mutate(no = 1:nrow(.)) %>%
-    mutate(theoretical = qgevd(no/(max(no) + 1), par1, par2, par3),
-	   empirical = quantile(maximum, no/(max(no) + 1))) %>%
+    dplyr::mutate(par1 = fittedGEV$parameters[1],
+	         par2 = fittedGEV$parameters[2],
+	         par3 = fittedGEV$parameters[3]) %>%
+    dplyr::filter(is.finite(maximum)) %>%
+    dplyr::arrange(maximum) %>%
+    dplyr::mutate(no = 1:nrow(.)) %>%
+    dplyr::mutate(theoretical = qgevd(no/(max(no) + 1), par1, par2, par3),
+	                empirical = quantile(maximum, no/(max(no) + 1))) %>%
     ggplot(aes(x = theoretical, y = empirical)) +
       geom_point() +
       theme_bw()
@@ -31,17 +61,17 @@ qqPlotGev <- function(sourceFrame, fittedGEV) {
 
 cdfsGev <- function(sourceFrame, fittedGEV) {
   sourceFrame %>%
-    mutate(par1 = fittedGEV$parameters[1],
+    dplyr::mutate(par1 = fittedGEV$parameters[1],
            par2 = fittedGEV$parameters[2],
            par3 = fittedGEV$parameters[3]) %>%
-    filter(is.finite(maximum)) %>%
-    arrange(maximum) %>%
-    mutate(no = 1:nrow(.)) %>%
-    mutate(theoretical = pgevd(maximum, par1, par2, par3),
+    dplyr::filter(is.finite(maximum)) %>%
+    dplyr::arrange(maximum) %>%
+    dplyr::mutate(no = 1:nrow(.)) %>%
+    dplyr::mutate(theoretical = pgevd(maximum, par1, par2, par3),
            empirical = no/max(no)) %>%
-    gather(CDF, value, theoretical, empirical) %>% 
+    tidyr::gather(CDF, value, theoretical, empirical) %>% 
     ggplot(aes(x = maximum, y = value, color = CDF)) +
-    geom_point() +
-    theme_bw() +
-    ylab("")
+      geom_point() +
+      theme_bw() +
+      ylab("")
 }
