1b733bcb1e377af2629e3d247390ed93a700d144
mstaniak <mateusz.staniak@math.uni.wroc.pl>
2017-05-29 22:52:25 +0200

    symmetric distribution

--- R/kendDist.R
+++ R/kendDist.R
@@ -102,3 +102,62 @@ rkend <- function(mAlpha) {
     })
   }
 }
+
+
+#' CDF of symmetrical Kendall stable distribution
+#' 
+#' @param mAlpha function giving moments of order alpha of step dist.
+#' 
+#' @return function function giving values of CDF of Kendall stable distribution
+#' 
+#' @export
+#' 
+#' @examples 
+#' pKend <- pkendSym(function(x) 1)
+#' # Step distribution: delta_{1}
+#' pKendall <- dKend(1:10, 0.5)
+#' # Values of CDF for arguments 1:10 and alpha = 0.5
+#' 
+
+pkendSym <- function(mAlpha) {
+  force(mAlpha)
+  function(x, alpha) {
+    Ft <- function(y) {
+      0.5*(1 + mAlpha(alpha)*(y^((-1)*alpha)) + exp(mAlpha(alpha)*(y^((-1)*alpha))))*exp((-1)*(y^((-1)*alpha)))
+    }
+    sapply(x, function(y) {
+      if(!is.finite(y)) return(NA)
+      else if(y == 0) 0.5
+      else if(y > 0) Ft(y)
+      else 1 - Ft((-1)*y)
+    })
+  }
+}
+
+
+#' Quantiles of symmetrical Kendall stable distribution
+#' 
+#' @param mAlpha function giving moments of order alpha of step dist.
+#' 
+#' @return function function returning quantiles of given orders
+#' 
+#' @export
+#' 
+#' @examples 
+#' qKend <- qkendSym(function(x) 1)
+#' # Step distribution: delta_{1}
+#' qKendall <- qKend(c(0.1, 0.9), 0.5)
+#' # Quantiles of order 0.1 and 0.9 for alpha = 0.5
+#' 
+
+qkendSym <- function(mAlpha) {
+  force(mAlpha)
+  function(p, alpha) {
+    oCDF <- function(x) pkendSym(mAlpha)(x, alpha)
+    sapply(p, function(q) {
+      if(!is.finite(q)) return(NA)
+      else if(q >= 0.5) uniroot({function(x) oCDF(x) - q}, lower = 0, upper = 10^80)$root
+      else (-1)*uniroot({function(x) oCDF(x) - (1 - q)}, lower = 0, upper = 10^80)$root
+    })
+  }
+}
