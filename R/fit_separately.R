c84d4fec509366b8d9b44c2c275d21ee9179d361
Mateusz Staniak <mateusz.staniak@math.uni.wroc.pl>
2018-04-26 13:37:04 +0200

    initial point for algorithm as a parameter

--- R/fit_separately.R
+++ R/fit_separately.R
@@ -14,7 +14,8 @@ kendall_loglik <- function(alpha, x) {
 
 #' Optimize log-likelihood to find distribution parameters
 #'
-#' @param data numeric vector of observation
+#' @param data numeric vector of observation.
+#' @param initial_point Initial guess for true parameters.
 #'
 #' @return Function of one argument of length 3 (alpha, location, scale)
 #'         which will be used to solve nonlinear system of equations
@@ -23,7 +24,7 @@ kendall_loglik <- function(alpha, x) {
 #' @export
 #'
 
-estimate_parameters <- function(data) {
+estimate_parameters <- function(data, initial_point) {
    fun <- function(parameters) {
      n <- length(data)
      c(n/parameters[1] +
@@ -38,7 +39,7 @@ estimate_parameters <- function(data) {
 
      )
    }
-   nleqslv::nleqslv(c(0.6, 0, 1), fun)$x
+   nleqslv::nleqslv(initial_point, fun)$x
 }
 
 #' Fit stable Kendall distribution for given data and m_alpha function.
@@ -46,14 +47,15 @@ estimate_parameters <- function(data) {
 #' @param data Numeric vector of observation to which the distribution will be fitted.
 #' @param m_alpha m_alpha function which will be used.
 #' @param quantiles vector of quantile range to be estimated.
+#' @param initial_point Initial guess for true parameters.
 #'
 #' @return fitted quantiles
 #'
 #' @export
 #'
 
-fit_kendall <- function(data, m_alpha, quantiles) {
-  to_estimate <- estimate_parameters(data)
+fit_kendall <- function(data, m_alpha, quantiles, initial_point) {
+  to_estimate <- estimate_parameters(data, initial_point)
   alpha <- to_estimate[1]
   loc <- to_estimate[2]
   scale <- to_estimate_scale[3]
@@ -71,13 +73,14 @@ fit_kendall <- function(data, m_alpha, quantiles) {
 #' @param data Numeric vector. Observation to which the distribution will be fitted.
 #' @param m_alpha m_alpha function to be used
 #' @param separation_point Order above which data (quantiles) will be separated.
+#' @param initial_points Initial guesses for true parameters.
 #'
 #' @return Empirical and estimated quantiles in a data frame.
 #'
 #' @export
 #'
 
-fit_separate <- function(data, m_alpha, separation_point) {
+fit_separate <- function(data, m_alpha, separation_point, initial_points) {
   data <- data[is.finite(data) & !is.na(data)]
   n_obs <- length(data)
   if(n_obs == 0) {
@@ -90,8 +93,8 @@ fit_separate <- function(data, m_alpha, separation_point) {
   upper_quantiles <- all_quantiles[all_quantiles > separation_point]
   lower_dataset <- data[data <= separate]
   upper_dataset <- data[data > separate]
-  all_lower_fit <- fit_kendall(lower_dataset, m_alpha, lower_quantiles)
-  all_upper_fit <- fit_kendall(upper_dataset, m_alpha, upper_quantiles)
+  all_lower_fit <- fit_kendall(lower_dataset, m_alpha, lower_quantiles, initial_points[[1]])
+  all_upper_fit <- fit_kendall(upper_dataset, m_alpha, upper_quantiles, initial_points[[2]])
   lower_fitted<- unlist(all_lower_fit[[1]],
                         use.names = F)
   upper_fitted <- unlist(all_upper_fit[[1]],
