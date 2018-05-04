d2e38988c9430e8cdcf72d1d844183f9e21e4e71
Mateusz Staniak <mateusz.staniak@math.uni.wroc.pl>
2018-05-04 19:10:15 +0200

    tests for stable dist.

--- tests/testthat/test_dists.R
+++ tests/testthat/test_dists.R
@@ -1,8 +1,31 @@
 context("Check function related to stable Kendall distribution")
 
 pKend <- pkend(function(x) 1)
-testthat::test_that("Check that support is on positive numbers", {
+qKend <- qkend(function(x) 1)
+pKendSym <- pkendSym(function(x) 1)
+qKendSym <- qkendSym(function(x) 1)
+
+testthat::test_that("Support is on positive numbers", {
   testthat::expect_equal(pKend(c(-20, -10, 0), 0.5), rep(0, 3))
 
 })
+
+testthat::test_that("Quantiles are computed correctly", {
+  testthat::expect_equal(pKend(qKend(c(0.1, 0.5, 0.9), 0.5), 0.5), c(0.1, 0.5, 0.9),
+                         tolerance = 1e-4)
+})
+
+testthat::test_that("Symmetric distribution is in fact symmetric", {
+  testthat::expect_equal(pKendSym(-2, 0.5), 1 - pKendSym(2, 0.5),
+                         tolerance = 1e-4)
+})
+
+
+testthat::test_that("Quantiles of symmetric distribution are computed correctly", {
+  testthat::expect_equal(pKendSym(qKendSym(c(0.1, 0.5, 0.9), 0.5), 0.5),
+                         c(0.1, 0.5, 0.9),
+                         tolerance = 1e-4)
+
+})
+
 
